91469025ccea7c20860711b160a2ee43dc7146f3
Shivam Sinha <shivams.sinha0@gmail.com>
2023-02-24 19:07:41 +0530

    Added Show Images in UI

--- src/app/show-product-details/show-product-details.component.ts
+++ src/app/show-product-details/show-product-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { map } from 'rxjs';
+import { ImageProcessingService } from '../image-processing.service';
+import { ShowProductImagesDialogComponent } from '../show-product-images-dialog/show-product-images-dialog.component';
 import { Product } from '../_model/product.model';
 import { ProductService } from '../_Services/product.service';
 
@@ -9,16 +13,22 @@ import { ProductService } from '../_Services/product.service';
 })
 export class ShowProductDetailsComponent {
 
-  displayedColumns: string[] = ['Id', 'Product Name', 'Product Description','Product Discounted Price', 'Product Actual Price' , 'Edit', 'Delete'];
+  displayedColumns: string[] = ['Id', 'Product Name', 'Product Description','Product Discounted Price', 'Product Actual Price' ,'Images', 'Edit', 'Delete'];
   productDetails:Product[] = [];
 
-  constructor(private productService:ProductService) {}
+  constructor(private productService:ProductService,
+    public imagesDialog:MatDialog,
+    private imageProcessingService:ImageProcessingService) {}
 
   ngOnInit() : void{
     this.getAllProducts();
   }
   public getAllProducts() {
-    this.productService.getAllProducts().subscribe({
+    this.productService.getAllProducts().
+    pipe(
+      map((x : Product[],i) => x.map((product:Product) => this.imageProcessingService.createImages(product))
+        )).
+    subscribe({
       next: (response:Product[]) =>  {console.log(response);
         this.productDetails = response;
       },
@@ -35,4 +45,14 @@ export class ShowProductDetailsComponent {
       complete:() => console.log("Completed")
     });
   }
-}
+  showImages(product:Product){
+    console.log(product);
+    this.imagesDialog.open(ShowProductImagesDialogComponent, {
+      data: {
+        images: product.productImages
+      },
+      height:'500px',
+      width: '800px'
+    });
+  }
+}   
